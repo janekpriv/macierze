65028f582c07053f6e7fa24a3287e4859b60ef76
janek <janekpriv@gmail.com>
2024-12-13 11:46:56 +0100

    troche gaussa

--- lmp9/trunk/src/gauss.c
+++ lmp9/trunk/src/gauss.c
@@ -5,9 +5,29 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+    
+	int rows = mat->r;
+	int columns = mat->c;
+
+	int reduced_rows;
+	if (rows>colums){
+		reduced_rows = columns;
+	}else{
+		reduced_rows = rows;
+	}
+	for(int i=0; i<reduced_rows){
+		for(int j=0; j<columns; j++){
+			
+
+
+		}
+
+
+
+	}
+
+
+  	
 
 		return 0;
 }
