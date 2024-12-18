0d823cda30bd6951a4502f873da993acbb5730cd
Janek <janekpriv@gmail.com>
2024-12-18 10:37:08 +0100

    Dodana funkcjonalnosc redukcji, obsługa macierzy nie kwadratowych, problemy  z weryfikacja macierzy osobliwej (if statement)

--- lmp9/trunk/src/gauss.c
+++ lmp9/trunk/src/gauss.c
@@ -1,4 +1,5 @@
 #include "gauss.h"
+#include "stdio.h"
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -8,27 +9,29 @@ int eliminate(Matrix *mat, Matrix *b){
     
 	int rows = mat->r;
 	int columns = mat->c;
+	int diagonal = 0; // mierzy przekatna maciezy (pomijamy komorki w ktorych dokonalismy redukcji)
 
-	int reduced_rows;
-	if (rows>colums){
+
+	int reduced_rows; // obsluga maciezy nie kwadratowych 
+	if (rows>columns){
 		reduced_rows = columns;
 	}else{
 		reduced_rows = rows;
 	}
-	for(int i=0; i<reduced_rows){
-		for(int j=0; j<columns; j++){
-			
-
-
+	for(int k=0; k<reduced_rows; k++){
+		for(int i=1; i<rows; i++){
+			if (mat->data[i][diagonal] == 0){
+				fprintf(stderr, "macierz osobliwa, dzielenie przez zero" );
+				return 1
+			}
+			double multiplier = mat->data[i-1][diagonal]/mat->data[i][diagonal];
+			for(int j=0; j<columns; j++){
+				mat->data[i][j] = mat->data[i][j] * multiplier;
+				mat->data[i][diagonal + j]-=mat->data[i-1][diagonal +j]; //odejmujemy wartosc z wiersza wyzej 
+			}
 		}
-
-
-
+		diagonal++;
 	}
-
-
-  	
-
 		return 0;
 }
 
