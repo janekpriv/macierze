32d9e4f0cf9fb052cfd174588a312fd751968f23
qbakom <01178382@pw.edu.pl>
2024-12-19 22:17:38 +0100

    implemented fully working gauss elimination algorithm

--- lmp9/trunk/src/gauss.c
+++ lmp9/trunk/src/gauss.c
@@ -1,33 +1,36 @@
 #include "gauss.h"
-
+#include <math.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
     
-	int rows = mat->r;
-	int columns = mat->c;
+	int n = mat->r;
 
-	int reduced_rows;
-	if (rows>colums){
-		reduced_rows = columns;
-	}else{
-		reduced_rows = rows;
-	}
-	for(int i=0; i<reduced_rows){
-		for(int j=0; j<columns; j++){
-			
+    for (int k = 0; k < n; k++) {
+        int max_row = k;
+        for (int i = k + 1; i < n; i++) {
+            if (fabs(mat->data[i][k]) > fabs(mat->data[max_row][k])) {
+                max_row = i;
+            }
+        }
+        double *temp = mat->data[k];
+        mat->data[k] = mat->data[max_row];
+        mat->data[max_row] = temp;
 
+        double tmp = b->data[k][0];
+        b->data[k][0] = b->data[max_row][0];
+        b->data[max_row][0] = tmp;
 
-		}
-
-
-
-	}
-
-
-  	
+        for (int i = k + 1; i < n; i++) {
+            double factor = mat->data[i][k] / mat->data[k][k];
+            for (int j = k; j < n; j++) {
+                mat->data[i][j] -= factor * mat->data[k][j];
+            }
+            b->data[i][0] -= factor * b->data[k][0];
+        }
+    }
 
 		return 0;
 }
